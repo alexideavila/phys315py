10f3ee6ac2b145989c57a477d3464aa4a714dd50
Alexi <nsfwlanguagelearning@gmail.com>
2019-11-02 09:34:51 -0700

    updatin

--- exercises7/program5.py
+++ exercises7/program5.py
@@ -7,13 +7,13 @@ for i in range(10):
 
 number3=number
 sqsum=0
-print()
+print() #formatting reasons
 print("And squares of those numbers are: ")
 for j in range(10):
 	print(number3, end="^2 ")
 	sqsum += number3**2
 	number3 += 1
 
-print()
-print("The sum of those numbers are: ", sqsum)
+print() #formatting reasons
+print("The sum of those numbers are:", sqsum)
 
